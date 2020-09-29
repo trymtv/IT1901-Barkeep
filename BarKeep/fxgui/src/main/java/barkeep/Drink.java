801c4120be25ede5bba61c55239e3b23fe4d4da5
TrymTv <tiltrym@gmail.com>
2020-09-29 16:50:45 +0200

    Modularize barkeep

---
+++ BarKeep/fxgui/src/main/java/barkeep/Drink.java
@@ -0,0 +1,94 @@
+package barkeep;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import json.BarKeepModule;
+import json.DrinkSerializer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+
+/**
+ * Class representing a Drink
+ */
+public class Drink {
+    private String name;
+    private int id;
+    private double value;
+
+    /**
+     * Get name of drink
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set name of drink
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get id of drink
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Get value of drink
+     * @return value
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Set value of Drink
+     * @param value
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    /**
+     * Constructor
+     * @param name
+     * @param value
+     */
+    public Drink(String name, float value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    public static void main(String[] args) throws JsonProcessingException {
+        Drink testDrink = new Drink("Brus", 5);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new BarKeepModule());
+        mapper.writeValueAsString(testDrink);
+        System.out.println(System.getProperty("user.home"));
+        try {
+            Writer writer =
+                    new FileWriter(Paths.get(System.getProperty("user.home"), "todolist.json").toFile(), StandardCharsets.UTF_8);
+            mapper.writeValue(writer, testDrink);
+        } catch (IOException e) {
+            System.err.println("Fikk ikke skrevet til todolist.json på hjemmeområdet");
+        }
+
+    }
+}
---
+++ BarKeep/fxgui/src/readme.md
@@ -0,0 +1,5 @@
+# Group gr2011 repository
+
+Welcome to IT1901 course fall 2020
+
+Good luck!
