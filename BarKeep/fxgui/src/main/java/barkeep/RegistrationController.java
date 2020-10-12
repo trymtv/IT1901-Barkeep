b95d80bfc13da3e1c016541f9de0b0b633472675
tormsk <tormsk@stud.ntnu.no>
2020-10-12 12:06:49 +0200

    before rebase

---
+++ BarKeep/fxgui/src/main/java/barkeep/RegistrationController.java
@@ -0,0 +1,46 @@
+package barkeep;
+
+import database.DatabaseUser;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class RegistrationController implements Initializable {
+
+    @FXML
+    TextField nameField;
+    @FXML
+    TextField usernameField;
+    @FXML
+    TextField emailField;
+    @FXML
+    TextField passwordField;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    public void handleRegistration(ActionEvent event) throws IOException {
+        User user = new User(nameField.getText(), usernameField.getText(), emailField.getText(), passwordField.getText());
+        try {
+            DatabaseUser.store(user);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    }
+
